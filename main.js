fca578c51e2c35185d2d609f732d043ad187f8f2
Park Sang Shin <34783156+pkiop@users.noreply.github.com>
2020-03-25 23:49:32 +0900

    .

--- main.js
+++ main.js
@@ -1,16 +1,51 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url')
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url; // 이건 query string 값을 받아온다는 것 같음.
+    var queryData = url.parse(_url, true).query;
+    var title = queryData.id;
+    var listmode = 'ul';
+    var filelink = 'data/' + title;
+    console.log(filelink);
+    fs.readFile(filelink, 'utf8', function(err, maintext){
+        var template = `
+<!doctype html>
+<html>
+<head>
+  <title>WEB1 - ${title}</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="/">WEB</a></h1>
+  <${listmode}>
+    <li><a href="/?id=HTML">HTML</a></li>
+    <li><a href="/?id=CSS">CSS</a></li>
+    <li><a href="/?id=JavaScript">JavaScript</a></li>
+  </${listmode}>
+  <h2>${title}</h2>
+  <p>
+  ${maintext}
+  </p>
+</body>
+</html>
+
+    `;
+    response.end(template);
+    });
+
+
+    console.log(queryData.id)
+    if(_url == '/'){
+      title = 'Welcome';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
+
+    
  
 });
 app.listen(3000);
