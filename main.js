15994d812b4b7eea347a42d8d3667b87868c169a
Park Sang Shin <34783156+pkiop@users.noreply.github.com>
2020-03-29 09:43:17 +0900

    .

--- main.js
+++ main.js
@@ -2,6 +2,34 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url')
 
+function templateHTML(title, data_list, body) {
+    return `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          ${data_list}
+          ${body}
+        </body>
+        </html>
+        `;
+}
+
+function templateList(listmode, filelist) {
+    var data_list = `<${listmode}>`;
+    var i = 0;
+    while(i < filelist.length) {
+        data_list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+        i = i + 1;
+    }
+    data_list += `</${listmode}>`
+    return data_list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url; // 이건 query string 값을 받아온다는 것 같음.
     var queryData = url.parse(_url, true).query;
@@ -18,64 +46,17 @@ var app = http.createServer(function(request,response){
                 console.log(filelist);
                 var title = 'Welcome';
                 var maintext = 'Hello, Node.js';
-                var data_list = `<${listmode}>`
-                var i = 0;
-                while(i < filelist.length) {
-                    data_list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-                    i = i + 1;
-                }
-                
-                data_list += `</${listmode}>`
-                var template = `
-                    <!doctype html>
-                    <html>
-                    <head>
-                      <title>WEB1 - ${title}</title>
-                      <meta charset="utf-8">
-                    </head>
-                    <body>
-                      <h1><a href="/">WEB</a></h1>
-                      ${data_list}
-                      <h2>${title}</h2>
-                      <p>
-                      ${maintext}
-                      </p>
-                    </body>
-                    </html>
-                    `;
+                var data_list = templateList(listmode, filelist);
+                var template = templateHTML(title, data_list, `<h2>${title}</h2>${maintext}`);
                 response.writeHead(200); // 잘 됐는지 
                 response.end(template);
             })
-
-            
         } else {
             fs.readFile(filelink, 'utf8', function(err, maintext){
                 fs.readdir('./data',function(error, filelist) {
-                    var data_list = `<${listmode}>`
-                    var i = 0;
-                    while(i < filelist.length) {
-                        data_list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-                        i = i + 1;
-                    }
-                    
-                    data_list += `</${listmode}>`
-                    var template = `
-                        <!doctype html>
-                        <html>
-                        <head>
-                          <title>WEB1 - ${title}</title>
-                          <meta charset="utf-8">
-                        </head>
-                        <body>
-                          <h1><a href="/">WEB</a></h1>
-                          ${data_list}
-                          <h2>${title}</h2>
-                          <p>
-                          ${maintext}
-                          </p>
-                        </body>
-                        </html>
-                        `;
+                    var data_list = templateList(listmode, filelist);
+                    var template = templateHTML(title, data_list, `<h2>${title}</h2>${maintext}`);
+
                     response.writeHead(200); // 잘 됐는지 
                     response.end(template);
                 });
---
+++ syntax/function2.js
@@ -0,0 +1,9 @@
+console.log(Math.round(1.6));
+console.log(Math.round(1.4));
+console.log(Math.round(1.5));
+
+console.log(sum(2, 4));
+function sum(first, second) {
+	console.log(first + second);
+	return first + second;
+}
