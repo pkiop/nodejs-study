21548f12755ab4b68aef58b40d64b89e408e76cb
Park Sang Shin <34783156+pkiop@users.noreply.github.com>
2020-03-31 22:15:50 +0900

    .

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url')
 var qs = require('querystring'); //qs -> querystring 이라는 모듈을 가져온다.
 
-function templateHTML(title, data_list, body) {
+function templateHTML(title, data_list, body, control) {
     return `
         <!doctype html>
         <html>
@@ -14,7 +14,7 @@ function templateHTML(title, data_list, body) {
         <body>
           <h1><a href="/">WEB</a></h1>
           ${data_list}
-          <a href="/create">create</a>
+          ${control}
           ${body}
         </body>
         </html>
@@ -50,7 +50,10 @@ var app = http.createServer(function(request,response){
                 var title = 'Welcome';
                 var maintext = 'Hello, Node.js';
                 var data_list = templateList(listmode, filelist);
-                var template = templateHTML(title, data_list, `<h2>${title}</h2>${maintext}`);
+                var template = templateHTML(title, data_list, 
+                    `<h2>${title}</h2>${maintext}`,
+                    `<a href="/create">create</a>`
+                    );
                 response.writeHead(200); // 잘 됐는지 
                 response.end(template);
             })
@@ -58,7 +61,17 @@ var app = http.createServer(function(request,response){
             fs.readFile(filelink, 'utf8', function(err, maintext){
                 fs.readdir('./data',function(error, filelist) {
                     var data_list = templateList(listmode, filelist);
-                    var template = templateHTML(title, data_list, `<h2>${title}</h2>${maintext}`);
+                    var template = templateHTML(title, data_list, 
+                    `<h2>${title}</h2>${maintext}`,
+                    `   <a href="/create">create</a> 
+                        <a href="/update?id=${title}">update</a>
+                        <!-- 주석 민감한 건 링크로 처리하면 절때로 안됨 <a href="/delete?id=${title}">delete</a> -->
+                        <form action="/delete_process" method="post">
+                            <input type="hidden" name="id" value="${title}">
+                            <input type="submit" value="delete">
+                        </form>
+                    `
+                    );
 
                     response.writeHead(200); // 잘 됐는지 
                     response.end(template);
@@ -67,13 +80,13 @@ var app = http.createServer(function(request,response){
         }
     } else if (pathname === '/create') {
         console.log("in create");
-        fs.readdir('./data',function(error, filelist) {
+        fs.readdir('./data', function(error, filelist) {
                 console.log(filelist);
                 var title = 'Welcome';
                 var maintext = 'Hello, Node.js';
                 var data_list = templateList(listmode, filelist);
                 var template = templateHTML(title, data_list, `
-                    <form action="http://localhost:3000/create_process" method="post">
+                    <form action="/create_process" method="post">
                         <p><input type="text" name="title" placeholder="title"></p>
                         <p>
                             <textarea name="description" placeholder="contents"></textarea>
@@ -82,8 +95,7 @@ var app = http.createServer(function(request,response){
                             <input type="submit">
                         </p>
                     </form>
-
-                `);
+                `, '');
                 response.writeHead(200); // 잘 됐는지 
                 response.end(template);
             })
@@ -104,6 +116,68 @@ var app = http.createServer(function(request,response){
             });
         });
         
+    } else if (pathname === '/update') {
+        fs.readFile(filelink, 'utf8', function(err, maintext){
+            fs.readdir(`./data`,function(error, filelist) {
+                console.log(queryData.id);
+                console.log("log :: ");
+                console.log(filelist);
+                var data_list = templateList(listmode, filelist);
+                var template = templateHTML(title, data_list, 
+                    //hidden 보이진 않지만 그 id와 value를 통해 값이 가긴 한다.
+                `
+                    <form action="/update_process" method="post">
+                        <input type="hidden" name="id" value="${title}">
+                        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                        <p>
+                            <textarea name="description" placeholder="contents">${maintext}</textarea>
+                        </p>
+                        <p>
+                            <input type="submit">
+                        </p>
+                    </form>
+                `,
+                `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                );
+
+                response.writeHead(200); // 잘 됐는지 
+                response.end(template);
+            });
+        });
+    } else if(pathname === `/update_process`) {
+        var body = '';
+        request.on('data', function(data) { // post 데이터가 많으면 그냥 꺼질수도. 조각조각 수신할떄마다 얻어오는 것 
+            body = body + data; // 받아올때마다 전송해받는 것 
+        });
+        request.on('end',function() { // 'end'는 그 조각조각 전송이 끝났다는 것
+            var post = qs.parse(body);
+            console.log("post :: ");
+            console.log(post);
+            var id = post.id;
+            var title = post.title;
+            var description = post.description;
+            fs.rename(`data/${id}`, `data/${title}`, function(error) {
+                fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+                    response.writeHead(302, {Location: `/?id=${title}`}); // 잘 됐는지 
+                    response.end('success');
+                });
+            });
+            
+        });
+    } else if(pathname === `/delete_process`) {
+        var body = '';
+        request.on('data', function(data) { // post 데이터가 많으면 그냥 꺼질수도. 조각조각 수신할떄마다 얻어오는 것 
+            body = body + data; // 받아올때마다 전송해받는 것 
+        });
+        request.on('end',function() { // 'end'는 그 조각조각 전송이 끝났다는 것
+            var post = qs.parse(body);
+            var id = post.id;
+            fs.unlink(`data/${id}`, function(error) {
+                response.writeHead(302, {Location: `/`}); // 잘 됐는지 
+                response.end();
+            })
+            
+        });
     } else {
         response.writeHead(404);
         response.end('Not found');
